482ca94874647798f9becb6d46383b1a896d8924
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-12 23:18:23 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+internal class Program
+{
+  static void Main()
+  {
+    for(int i = 15; i <= 25; i++)
+    {
+      if(i % 5 == 0)
+      {
+        Console.WriteLine(i);
+      }
+    }
+
+
+
+
+  }
+}
+
