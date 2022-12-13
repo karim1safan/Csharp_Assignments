17ff822e5b750b23dd457e37d85c295e08add45f
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-13 19:27:56 +0200

    Add files via upload
    Write a program that reads from the console a positive integer number 
    N (N < 20) and prints a matrix of numbers as on the figures below:

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+internal class Program
+{
+  static void Main()
+  {
+    // karim mahmoud safan | physics & CS | Level 01
+    int n;
+    do
+    {
+      Console.Write("Length: ");
+      n = int.Parse(Console.ReadLine());
+    }
+    while (n < 1);
+
+    if(n < 20)
+    {
+      for (int i = 1; i <= n; i++)
+      {
+        for (int j = 0; j <= n - 1; j++)
+        {
+          Console.Write(i + j);
+        }
+        Console.WriteLine();
+      }
+    }
+    else
+    {
+      Console.WriteLine("Enter the value less than 20");
+    }
+  }
+}
