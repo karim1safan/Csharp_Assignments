464baf0b39e5de9cf1c14498c040e9a58a74ef31
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-13 15:53:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+internal class Program
+{
+  static void Main()
+  {
+    // karim mahmoud safan | Physics & CS | Level 01
+    Console.Write("Enter a number: ");
+    int num = int.Parse(Console.ReadLine());
+
+    if(num > 1 && num < 100)
+    {
+      for(int i = 2; i < num; i++)
+      {
+        if(num % i == 0)
+        {
+          Console.WriteLine("{0} is not prime.",num);
+          return; // to exist from loop when find the number.
+        }
+      }
+      Console.WriteLine("{0} is prime.", num);
+    }
+  }
+}
+// prime number accept on itself and 1.
+// 7 > 2 - 6 = is prime.
+// 8 > 2 - 7 = isn't prime accept on 4.
