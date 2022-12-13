a2905db503347e95b763cb39a04429981ca93feb
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-13 19:36:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+internal class Program
+{
+  static void Main()
+  {
+    // karim mahmoud safan | physics & CS | Level 01
+    Console.WriteLine("Each element multiplied by 5");
+    int[] arr = new int[20];
+    
+    for(int i = 0; i < arr.Length; i++)
+    {
+      arr[i] = i * 5;
+      Console.WriteLine(arr[i]);
+    }
+  }
+}
+
