48abc7f5c8f0c8c8b971dbbbd6e3df5b79a38eb7
Karim Safan <110535652+karim1safan@users.noreply.github.com>
2022-12-14 13:07:30 +0200

    Add files via upload
    Write a program that for a given integers n and x, calculates the sum: 
    n
    x
    n
    x x
    S
    !
    ...
    !1 !2 1  2  

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+internal class Program
+{
+  static void Main()
+  {
+    // karim mahmoud safan | physics & CS | level 01
+    Console.Write("Enter N: ");
+    int n = int.Parse(Console.ReadLine()); // 2
+    Console.Write("Enter X: ");
+    int x = int.Parse(Console.ReadLine());// 2
+
+    double sum = 0;
+    int fact = 1;
+
+    for(int i = 1; i <= n; i++)
+    {
+      fact = fact * i;
+      sum += fact / Math.Pow(x, i);
+    }
+
+    Console.WriteLine(sum + 1);
+  }
+}
+
